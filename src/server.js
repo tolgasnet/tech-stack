6d990e4d5689bcaf68f6ea80d7c170593187a7f6
Tolga Sofuoglu <noreply@github.com>
2022-08-04 14:07:10 +0300

    Rename app.js to server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const app = express()
+const server = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+server.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
